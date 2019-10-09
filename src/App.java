0ae2403fe9ec0de575b1097782df8c5d19ee613b
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-09 17:42:43 -0300

    App agregado.

--- src/App.java
+++ src/App.java
@@ -1,22 +1,47 @@
 import ucn.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class App {
-	//Funciones: 
-	
-	public static void cargarArchivos() {
+	//Comprobar Fecha:
+	public static void ComprobarFecha() {
+		Date fecha = new Date();
+		SimpleDateFormat timeFormat = new SimpleDateFormat("dd/MM/yyyy");
+		String todayDay = timeFormat.format(fecha);
 		
+		StdOut.println("Ingrese la fecha actual (dd/mm/yyyy");
+		String DateInsert = StdIn.readString();
 	}
-	public static void menu() {
-		//Declaracion de variables:
-		date fecha =0;
-		StdOut.println("¡Bienvenido al sistema SUSTO!");
-		StdOut.println("Ingrese la fecha actual (DD/;MM/AAAA) :");
-		fecha =StdIn.readInt();
-		int salir =0;
-		while(salir){
-			
+	public static void VerificarFecha() {
+		try {
+			String fechaIngreso ="30/02/2016";
+			Date fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fechaIngreso);
+			String fechaSalida = new SimpleDateFormat("dd/MM/yyyy").format(fecha);
+			StdOut.println ("ingreso: "+fechaIngreso);
+			StdOut.println(fecha);
+			StdOut.println(fechaSalida);	
+			StdOut.println((fechaIngreso.equals(fechaSalida))?"fecha correcta":"fecha incorrecta");
+		} catch (Exception e) {
+			if
 		}
 	}
+	//Funciones: 
+	public static void menu() {
+		//Declaracion de variables:
+		StdOut.println("¡Bienvenido al sistema SUSTO!");
+		VerificarFecha();//Verificamos fecha ingresada..
+		//Opcion MENU:
+		int opcion=0;
+		while(opcion!= 6) {
+			StdOut.println("1. Cargar Archivos \n2. Crear Nuevas Entidades \n3. Registrar Ingreso y Salida \n4. Reasignar Científico \n5. Reportes de Personal y Costos \n6. Cierre de Sistema");
+			opcion = StdIn.readInt();
+		}
+	
+	}
 	public static void main(String[] args) {
+		menu();
 		
 	}
 
---
+++ src/SistemaSUSTO.java
@@ -0,0 +1,6 @@
+
+public interface SistemaSUSTO {
+	public boolean CrearInstalacion(String NombreInstalacion, int CantidadDptos);//String NombreDpto, int CapacidadDpto
+	public boolean ContratarCientifico(String Rut,String Nombre, String ApellidoP,String ApellidoM,String Area,int CostoAsociado );
+	
+}
