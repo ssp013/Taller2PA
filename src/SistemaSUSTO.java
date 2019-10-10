2d847cb4947ee58a339ae848a320be055d5418f3
sando2345 <56320783+sando2345@users.noreply.github.com>
2019-10-09 22:04:57 -0300

    Update SistemaSUSTO.java

--- src/SistemaSUSTO.java
+++ src/SistemaSUSTO.java
@@ -2,5 +2,7 @@
 public interface SistemaSUSTO {
 	public boolean CrearInstalacion(String NombreInstalacion, int CantidadDptos);//String NombreDpto, int CapacidadDpto
 	public boolean ContratarCientifico(String Rut,String Nombre, String ApellidoP,String ApellidoM,String Area,int CostoAsociado );
-	
+	public boolean RegistrarIngreso(String Instalacion, String rutCientifico, String fecha, String hora);
+	public boolean RegistrarSalida(String Instalacion, String rutCientifico, String fecha, String hora);
+	public boolean ReasignarCientifico(String OpcionCambio);
 }
