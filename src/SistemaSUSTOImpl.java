6d033ebaca7d2401d0795eb59ccc7e20b5d8146d
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-10 17:58:15 -0300

    D

--- src/SistemaSUSTOImpl.java
+++ src/SistemaSUSTOImpl.java
@@ -7,6 +7,14 @@ import ucn.*;
  * @author TomasSandoval Sebastian Sanchez
  */
 public class SistemaSUSTOImpl implements SistemaSUSTO {
+    private ListaCientificos listaCientificos;
+    private ListaDptos listaDptos;
+    private ListaProyectos listaProyectos;
+    private ListaInsta listaInsta;
+    //private ListaAsociacionCP listaAsociacionCP;
+    //private ListaAsociacionCI listaAsociacionCI;
+    private ListaIngresos listaIngresos;
+    private ListaSalidas listaSalidas;
 
 	private String dateFormat;
 	@Override
