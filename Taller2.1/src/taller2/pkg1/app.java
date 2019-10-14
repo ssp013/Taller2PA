80c976074c1dab5518efcb9c0aa33b40233fc61b
Tomas Sandoval <Tomas Sandoval@LAPTOP-G01OAQ2U>
2019-10-14 19:00:30 -0300

    registrar ingreso registrar salida reasignar cientifico proyecto reasignar cientifico instalacion

--- Taller2.1/src/taller2/pkg1/app.java
+++ Taller2.1/src/taller2/pkg1/app.java
@@ -2,34 +2,75 @@
 package taller2.pkg1;
 import ucn.*;
 public class app {
-    public static void main(String[]args){
-        
+    public void ListadoPersonalInsta(sistemaSUSTO sistema){
         ListaProyectos listaP = returnListaProyectos();
         ListaCientificos listaC = returnListaCient();
-        StdOut.println("ingrese codigo proyecto: ");
-        String code = StdIn.readString();
-        for(int i=0;i<listaP.getCantProyectos();i++){
-            Proyecto proy = listaP.getProyectoI(i);
-            if(proy.getCodigoProyecto.equals(code)){
-                StdOut.println("Proyecto: "+proy.getNombreProyecto());
-                for(int j=0;j<ListaC.getCantCientificos();j++){
-                    Cientifico cient = ListaC.getCientificoI(j);
-                    String nombre = cient.getNombre();
-                    String ApellidoP = cient.getApellidoP();
-                    ListaProyectosCient LPC = cient.getListaProyectosCient;
-                    for(int k=0;k<LPC.getCantProyectosCient;k++){
-                        Proyecto proy1 = LPC.getProyectoCientI(k);
-                        if(proy1.getNombreProyecto().equals(proy.getNombreProyecto())){
-                            StdOut.println(nombre+" "+ApellidoP);
+        ListaDptos ListaD = returnListaDptos();
+        ListaInsta ListaI = returnListaInsta();
+        
+        for(int i=0;i<ListaI.getCantInsta();i++){
+            Instalaciones insta = ListaI.getInstalacionI(i);
+            StdOut.println("Instalacion: "+insta.getNombreInstalacion());
+            ListaDepartamentoInstalacion listaDepaIn = insta.getListaDepartamentoInstalacion();
+            for(int j=0;j<listaDepaIn.getCantDeptos();j++){
+                Departamendo dep = listaDepaIn.getDepartamentoI(j);
+                StdOut.println("Departamento(s): "+dep.getNombreDpto;);
+                for(int k=0;k<listaP.getCantProyectos();k++){
+                    Proyecto proy = listaP.getProyectoI(k);
+                    if(proy.getDptoResponsable().equals(dep.getNombreDpto())){
+                        StdOut.println("Proyecto(s): "+proy.getNombreProyecto());
+                        for(int l=0;l<listaC.getCantCientificos();l++){
+                            Cientifico cient = listaC.getCientificoI(l);
+                            ListaProyectosCient listaProyC = cient.getListaProyectosCient();
+                            for(int m=0;m<listaProyC.getCantProyectosCient();m++){
+                                Proyecto proy1 = listaProyC.getProyectoCientI(m);
+                                if(proy1.getNombreProyecto().equals(proy.getNombreProyecto())){
+                                    StdOut.println("Personal: "+cient.getNombre()+" "+cient.getApellidoP());
+                                }
+                            }
                         }
                     }
                 }
             }
+            
         }
-                
-               
         
     }
+    
+    
+    public static void main(String[]args){
+        
+        ListaProyectos listaP = returnListaProyectos();
+        ListaCientificos listaC = returnListaCient();
+        ListaDptos ListaD = returnListaDptos();
+        ListaInsta ListaI = returnListaInsta();
+        for(int i=0;i<listaP.getCantProyectos();i++){
+            Proyecto proy = listaP.getProyectoI(i);
+            StdOut.println("Proyecto: "+proy.getNombreProyecto());
+            for(int j=0;j<ListaC.getCantCientificos();j++){
+                Cientifico cient = ListaC.getCientificoI(j);
+                String nombre = cient.getNombre();
+                String ApellidoP = cient.getApellidoP();
+                ListaProyectosCient LPC = cient.getListaProyectosCient;
+                for(int k=0;k<LPC.getCantProyectosCient;k++){
+                    Proyecto proy1 = LPC.getProyectoCientI(k);
+                    if(proy1.getNombreProyecto().equals(proy.getNombreProyecto())){
+                        StdOut.println(nombre+" "+ApellidoP);
+                    }
+                }
+            }
+            
+        }//FIN DEL PRIMERO FOR
+        
+       
+            
+        
+        
+           
+        
+    }
+    
+    
     
     
 
